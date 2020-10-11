4d8c60beba1f2c361864917ffef706152ab673ae
jrwhiteh <jrwhiteh@buffalo.edu>
2020-10-11 00:22:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+#Consider a classification problem where we are building a logistic regression classifier. The task is to predict if a given city has a risk of a disease epidemic or not.
+
+import numpy as np
+
+# w = weight vector for logistic regression
+w = np.array([])
